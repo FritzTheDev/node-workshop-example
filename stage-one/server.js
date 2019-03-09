a578660f6f1703f548d6285fecea9c2d9b6f897c
ottovbismarck <fritzj96@gmail.com>
2019-03-09 14:51:09 -0800

    commenting up the server + a missing semicolon

--- stage-one/server.js
+++ stage-one/server.js
@@ -2,15 +2,21 @@
 const express = require('express');
 // to use express, you've got to instantiate it
 const app = express();
-//db here until I use something better.
+// db here until we get into MongoDB
 const db = require('./dummy-db');
-
+// "GET" route that gets all the todos in our dummy JS database
+// first argument is a 'route' string, second is a callback with Request & Response
 app.get('/api/todos', (req, res) => {
+  // response status
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved',
     todos: db
   });
 });
+// constants that are used as config variables can be ALLCAPS-ed
+const PORT = 3000;
 
-const PORT = 3000 //consts that are used as config variables can be ALLCAPS-ed
+app.listen(PORT, () => {
+  console.log(`We Have Liftoff on port ${PORT}🚀`)
+})
