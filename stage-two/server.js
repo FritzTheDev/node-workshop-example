cf1f8ff680d93d199ecb7c5455a7a7da8cf4b1b9
ottovbismarck <fritzj96@gmail.com>
2019-03-09 15:22:53 -0800

    commented up server & added post route

--- stage-two/server.js
+++ stage-two/server.js
@@ -13,9 +13,38 @@ app.get('/api/todos', (req, res) => {
   res.status(200).send({
     success: 'true', // alerts the client app that the request suceeded
     message: 'todos retrieved', // same
-    todos: db //the actual "database" object that gets passed with the request as part of the JSON.
+    todos: db // the actual "database" object that gets passed with the request as part of the JSON.
   });
 });
+// post route to add data
+app.post('/api/todos', (req, res) => {
+  // interrupts the function to respond with status 400 if no title
+  if (!req.body.title) {
+    // 4xx is the "Problem with a request" error code
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    }); // same as above, but with description
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+  const todo = {
+    id: db.length + 1, // adds the db at the next available index
+    title: req.body.title, // adds title from request body
+    description: req.body.description // adds description from request description
+  }
+  db.push(todo);
+  // created new resource status code
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    todo: todo // could also be written as a single "todo" without the assignment.
+  });
+});
+
 // constants that are used as config variables can be ALLCAPS-ed
 const PORT = 3000;
 // makes the server start listening for requests.
